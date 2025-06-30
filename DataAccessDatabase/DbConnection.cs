4692b0d1616039b4cf28eaf4343015a4c851f63d
MemoriJemand <if23b021@technikum-wien.at>
2025-06-30 13:03:56 +0200

    base database connection

--- DataAccessDatabase/DbConnection.cs
+++ DataAccessDatabase/DbConnection.cs
@@ -1,10 +1,23 @@
-
+using Npgsql;
 
 namespace DataAccessDatabase
 {
     public class DbConnection
     {
-        
+        private string connectionInfo = "Host=http://127.0.0.1:5433;Database=tour_planner;Username=tour_server;Password=L9vCSjLsY07EAtwC;";
+        public readonly NpgsqlDataSource? dataBase;
+
+        public DbConnection(/*string connectionInfo*/)
+        {
+            try
+            {
+                dataBase = NpgsqlDataSource.Create(connectionInfo);
+            }
+            catch (NpgsqlException e)
+            {
+                Console.WriteLine("NpgSqlException: {0}", e);
+            }
+        }
     }
 
 }
